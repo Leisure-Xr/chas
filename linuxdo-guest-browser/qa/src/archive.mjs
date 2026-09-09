5a5f16c9d1df69675dc707ef8d5660044bc78c11
leisure <leisure@lexiao.org>
2026-09-10 02:32:51 +0800

    fix: support QA checks on Linux and Windows

--- linuxdo-guest-browser/qa/src/archive.mjs
+++ linuxdo-guest-browser/qa/src/archive.mjs
@@ -1,15 +1,20 @@
 import { runCommand } from './process.mjs';
 
 export async function listArchive(path) {
-  const { stdout } = await runCommand('tar', ['-tf', path]);
+  const { stdout } = process.platform === 'win32'
+    ? await runCommand('tar', ['-tf', path])
+    : await runCommand('unzip', ['-Z1', path]);
   return stdout.split(/\r?\n/).filter(Boolean);
 }
 
 export async function readArchiveEntry(path, entry) {
-  const { stdout } = await runCommand('tar', ['-xOf', path, entry]);
+  const { stdout } = process.platform === 'win32'
+    ? await runCommand('tar', ['-xOf', path, entry])
+    : await runCommand('unzip', ['-p', path, entry]);
   return stdout;
 }
 
 export async function extractArchive(path, destination) {
-  await runCommand('tar', ['-xf', path, '-C', destination]);
+  if (process.platform === 'win32') await runCommand('tar', ['-xf', path, '-C', destination]);
+  else await runCommand('unzip', ['-q', path, '-d', destination]);
 }
